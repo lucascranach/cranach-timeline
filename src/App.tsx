543185110ad60c25f98a822f223d76f6132edc86
Calvin Hinze <calvinhinzer@gmail.com>
2025-10-13 22:38:35 +0200

    add absolute title

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import "./styles/App.css"
 import Scene from "./components/Scene"
 import { Leva } from "leva"
 
+import styled from "styled-components"
+
+const Title = styled.h1`
+  position: absolute;
+  top: 3rem;
+  left: 3rem;
+  color: white;
+  font-size: 2rem;
+  font-weight: 100;
+  z-index: 10;
+  margin: 0;
+  padding: 0;
+`
+
 function App() {
   return (
     <>
+      <Title>Lucas Cranach Timeline</Title>
       <Leva collapsed />
+
       <Scene />
     </>
   )
